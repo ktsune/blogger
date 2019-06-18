36e0073e0076574578e49f795f2d8e7ae9f1de92
Sarah Tatro <statrox4@gmail.com>
2019-06-18 14:00:44 -0600

    Add user ability to make comment from show page

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -16,6 +16,8 @@ class ArticlesController < ApplicationController
   end
 
   def show
+    @comment = Comment.new
+    @comment.article_id = @article.id
   end
 
   def destroy
--- spec/features/user_sees_one_article_spec.rb
+++ spec/features/user_sees_one_article_spec.rb
@@ -1,9 +1,24 @@
 require "rails_helper"
 
 describe "user sees one article" do
+  describe "they fill in a comment form" do
+  it "displays the comment on the article show" do
+    article = Article.create!(title: " Title", body: "New Body")
+
+    visit article_path(article)
+
+    fill_in "comment[author_name]", with: "ME!"
+    fill_in "comment[body]", with: "So many thoughts on this article."
+    click_on "Submit"
+
+    expect(current_path).to eq(article_path(article))
+    expect(page).to have_content("Post a Comment")
+    expect(page).to have_content("ME!")
+    expect(page).to have_content("So many thoughts on this article.")
+  end
   describe "they link from the article index" do
     it "displays information for one article" do
-      article = Article.create!(title: "New Title", body: "New Body")
+      article = Article.create!(title: " Title", body: "New Body")
       comment_1 = article.comments.create(author_name: "Me", body: "Commenty comments")
       comment_2 = article.comments.create(author_name: "You", body: "So much to say")
 
@@ -17,6 +32,7 @@ describe "user sees one article" do
       expect(page).to have_content(comment_1.body)
       expect(page).to have_content(comment_2.author_name)
       expect(page).to have_content(comment_2.body)
+    end 
     end
   end
 end
